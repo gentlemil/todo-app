d70f3c1f2d459f46f465c873d9013c7d0beecbc5
Milosz Bukala <mil.bukala@gmail.com>
2020-05-07 19:59:14 +0200

    Refactoring App component to class components, add events state and implemented them in the code.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import './App.css';
-
+import React, { Component } from 'react';
 import Countdown from './Countdown';
 
-const App = () => {
-  return (
-    <div>
-      <Countdown name='breakfast' time='07:00' />
-      <Countdown name='lunch' time='15:00' />
-    </div>
-  )
+import './App.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      events: [
+        { id: 0, name: 'breakfast', time: '07:00' },
+        { id: 1, name: 'lunch', time: '15:00' },
+        { id: 2, name: 'practise', time: '20:00' },
+      ]
+    }
+  }
+  render() {
+    const events = this.state.events.map(el => {
+      return <Countdown name={el.name} time={el.time} />
+    });
+
+    return (
+      <div>
+        {events}
+      </div >
+    )
+  }
 }
 
 export default App;
